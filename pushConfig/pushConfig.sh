6a7c2a969bbe591cccab9233bdff12faaaf196a1
anokha777 <anokha777@gmail.com>
2018-08-17 16:46:19 +0530

    circle si token updated

--- pushConfig/pushConfig.sh
+++ pushConfig/pushConfig.sh
@@ -1,6 +1,6 @@
-curl --user ${CIRCLE_TOKEN}: \ 834e505e51f117270de0040820df3457f0f5728a
+curl --user ${CIRCLE_TOKEN}: \ 
     --request POST \
-    --form revision=<commit hash>\
+    --form revision=13f130c5230272dc7de6a8843c2f9eec6395fb89 \
     --form config=@config.yml \
     --form notify=false \
-        https://circleci.com/api/v1.1/project/<source, eg. github>/<user name>/<project name>/tree/<branch name>
+        https://circleci.com/api/v1.1/project/github/anokha777/CircleCI-V-2.0/tree/master
